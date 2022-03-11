611478cfdad6e68d2aaa8a9f368739eaa3430205
Hugo Karas <hkaras@ethz.ch>
2022-03-11 11:05:01 +0100

    Added Attenuation Tuneing

--- autoDeer/Tunning.py
+++ autoDeer/Tunning.py
@@ -12,13 +12,13 @@ from scipy.optimize import minimize_scalar
 MODULE_DIR = importlib.util.find_spec('autoDeer').submodule_search_locations
 
 
-def setup_pulse_trans(api,ps_length:tuple,d0):
+def setup_pulse_trans(api,ps_length:tuple,d0,channel:str = "MainPhase"):
 
     # Setting the location of the pulse_spel
     #def_name = '/home/xuser/Desktop/huka/autoDeer/autoDeer/PulseSpel/param_opt.def'
-    def_name = MODULE_DIR[0] + '/PulseSpel/param_opt.def'
+    def_name = MODULE_DIR[0] + '/PulseSpel/phase_set.def'
     #exp_name = '/home/xuser/Desktop/huka/autoDeer/autoDeer/PulseSpel/param_opt.exp'
-    exp_name = MODULE_DIR[0] + '/PulseSpel/param_opt.exp'
+    exp_name = MODULE_DIR[0] + '/PulseSpel/phase_set.exp'
     
     api.set_ReplaceMode(True) #Turn replace mode off
     api.set_PhaseCycle(False)
@@ -44,7 +44,7 @@ def setup_pulse_trans(api,ps_length:tuple,d0):
 
     # Selecting the experiment
     api.set_PulseSpel_experiment("Hahn Echo")
-    api.set_PulseSpel_phase_cycling("Hahn")
+    api.set_PulseSpel_phase_cycling(channel)
 
     api.set_Acquistion_mode(3)
     api.run_exp()
@@ -155,3 +155,53 @@ def tune(api,d0:int = 600,channel:str = 'main',phase_target:str = 'R+'):
     output = minimize_scalar(objecive,method='bounded',bounds=[lb,ub],args=(phase_aim,neg_aim),options={'xatol':0.05,'maxiter':30})
 
     return output.x
+
+
+def tune_power(api,d0:int = 600,channel:str = 'main') -> float:
+
+    channel_opts = ['main', '+<x>','-<x>','+<y>','-<y>']
+    if not channel in channel_opts:
+        raise ValueError(f'Channel must be one of: {channel_opts}')
+
+
+    setup_pulse_trans(api,(16,32),d0) #TODO auto-finding of do though the abs pulse
+
+    if channel == 'main':
+        lb = 0.0
+        ub = 60.0
+        atten_channel = 'PowerAtten'
+    elif channel in ['+<x>','-<x>','+<y>','-<y>']:
+        lb = 0.0
+        ub = 100.0
+        if channel == '+<x>':
+            atten_channel = 'BrXAmp'
+        elif channel == '-<x>':
+            atten_channel = 'MinBrXAmp'
+        elif channel == '+<y>':
+            atten_channel = 'BrYAmp'
+        elif channel == '-<y>':
+            atten_channel = 'MinBrYAmp'
+
+    def objecive(x,*args):
+        '''x is the given attenuator setting'''
+        api.hidden[atten_channel].value = x # Set phase to value
+        time.sleep(2)
+        api.run_exp()
+        while api.is_exp_running():
+            time.sleep(1)
+
+        data = api.acquire_scan()
+        t = data.time
+        v = data.data
+        v_cor = DC_cor(v)
+
+        inte = - np.sqrt(np.trapz(np.real(v_cor)^2) + np.trapz(np.imag(v_cor)^2))
+
+
+        # Calc Phase
+        print(f'Attenuator Setting = {x:.1f} \t inte = {inte:.2f} ')
+        return inte
+
+    output = minimize_scalar(objecive,method='bounded',bounds=[lb,ub],options={'xatol':20,'maxiter':30})
+
+    pass
