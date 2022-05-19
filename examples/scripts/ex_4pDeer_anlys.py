5ad4300a6648186035fe016495445832fd7a57ae
Hugo <hkaras@ethz.ch>
2022-05-19 16:16:25 +0200

    Add 4p DEER Analysis Example

--- examples/scripts/ex_4pDeer_anlys.py
+++ examples/scripts/ex_4pDeer_anlys.py
@@ -1,11 +1,21 @@
 # %%
 """ 
-Analysing 4pDEER experiments to deterine the ROI
+Analysing 4pDEER experiments to determine quality and the ROI
 -----------------------------------------------------------
 
+Here we look at how to quickly import a fine from bruker and abstract the MNR and the ROI.
 """
 
 
-import autoDeer as aD
+from autoDeer import std_4p_deer_analysis
+from deerlab import deerload
+import numpy as np
+
+t,V = deerload("../data/WALP23_H20_C7_C22_2Ddec_DEER_d1_1_7us_d2_2400_2hrs_long.DSC")
+
+ztime = t[np.argmax(V)] #If zerotime is not known it can be found
+
+std_4p_deer_analysis(t,V,tau1=1.7,tau2=2.4,zerotime = ztime)
 
 
+# %%
