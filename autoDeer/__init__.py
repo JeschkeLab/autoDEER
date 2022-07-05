2e6eaa6e50590d9d9fcc7ff988005562f50f2239
Hugo <hkaras@ethz.ch>
2022-07-05 12:12:20 +0200

    Adding the uwb_load function

--- autoDeer/__init__.py
+++ autoDeer/__init__.py
@@ -8,5 +8,5 @@ from .TwoD_Experiment import *
 from .tools import eprload
 from .FieldSweep import FieldSweep
 from .ResPro import resonatorProfile
-from .home_built_func import get_deer,find_AWG_deer_files
+from .home_built_func import get_deer,find_AWG_deer_files,uwb_load
 from .DEER_analysis import std_deer_analysis
