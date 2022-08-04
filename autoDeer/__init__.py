bd79bde1f6a05b137b02311db3b336cf47bb5b8a
Hugo Karas <hkaras@ethz.ch>
2022-08-04 09:02:12 +0200

    Removed Param Optimisation Code

--- autoDeer/__init__.py
+++ autoDeer/__init__.py
@@ -1,8 +1,8 @@
+import imp
 from ._version import __version__
 from .DEER_4p import *
 from .File_Saving import *
 from .Logging import *
-from .Param_Optimization import *
 from .Tunning import *
 from .TwoD_Experiment import *
 from .tools import eprload
@@ -10,3 +10,4 @@ from .FieldSweep import FieldSweep
 from .ResPro import resonatorProfile
 from .home_built_func import get_deer,find_AWG_deer_files,uwb_load,calc_perceived_freq
 from .DEER_analysis import std_deer_analysis
+from .Relaxation import Carr_Purcell
