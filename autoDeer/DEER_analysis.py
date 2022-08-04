7ada3d604bf2c638fdc55d725a22617ff52aa7f4
Hugo Karas <hkaras@ethz.ch>
2022-08-04 10:39:14 +0200

    Removing DEER_4p file. Simplifying fiel structure

--- autoDeer/DEER_analysis.py
+++ autoDeer/DEER_analysis.py
@@ -1,11 +1,15 @@
-import time
 import numpy as np
 from scipy.optimize import curve_fit
 import deerlab as dl
 import matplotlib.pyplot as plt
-from autoDeer.DEER_4p import IdentifyROI
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy.integrate import cumulative_trapezoid
+import logging
+import importlib
 
+log = logging.getLogger('core.DEER')
+
+
+MODULE_DIR = importlib.util.find_spec('autoDeer').submodule_search_locations
 
 
 def calc_identifiability(profile):
@@ -14,7 +18,6 @@ def calc_identifiability(profile):
     fit_func = lambda x,a,b,c: a * (x-b)**2 + c
     fit_result = curve_fit(fit_func,x,y,bounds=(0,np.inf),xtol=0.001)
     return fit_result[0][0]
-
 
 def std_deer_analysis(t:np.ndarray,V:np.ndarray,tau1,tau2,tau3=None,zerotime=0,num_points=50,compactness=True,precision="Detailed"):
     """std_deer_analysis This function conducts the standard deer analysis using deerlab
@@ -137,3 +140,54 @@ def std_deer_analysis(t:np.ndarray,V:np.ndarray,tau1,tau2,tau3=None,zerotime=0,n
 
     return fig,ROI,rec_tau_max
 
+def IdentifyROI(dist:np.ndarray,r:np.ndarray,criterion:float = 0.99,plot:bool=False):
+    """IdentifyROI _summary_
+
+    Parameters
+    ----------
+    dist : np.ndarray
+        The distance distribution.
+    r : np.ndarray
+        The distance axis
+    criterion : float, optional
+        What fraction of the distance distribution must be in the ROI, by default 0.99
+    plot : bool, optional
+        Plot the cumulative graphs, by default False
+    """
+
+    # Normlaize the distribution
+    dist = dist / np.trapz(dist,r)
+    # cumulative_dist = cumulative_trapezoid(dist,r,initial=0)
+    # min_dist = r[np.argmin(np.abs(1 - cumulative_dist - criterion))]
+    # max_dist = r[np.argmin(np.abs(cumulative_dist - criterion))]
+
+    c_trapz_dist = np.zeros((dist.shape[0],dist.shape[0]))
+
+    for i in range(0,dist.shape[0]):
+        c_trapz_dist[i,i:] = cumulative_trapezoid(dist[i:],r[i:],initial=0)
+
+    c_trapz_dist[(c_trapz_dist < criterion)] = 3
+    ind = np.unravel_index(np.argmin(c_trapz_dist),c_trapz_dist.shape)
+    min_dist = r[ind[0]]
+    max_dist = r[ind[1]]
+
+    # Enlarge ROI
+    width = max_dist - min_dist
+    max_dist = max_dist + width *0.25
+    min_dist = min_dist - width *0.25
+
+    if not plot:
+        return[min_dist,max_dist]
+
+    else:
+        pass
+
+def QualityControl(fit):
+
+    if fit.regparam < 1:
+        log.warning('Regularization Parameter is too small. Please try a different model')
+        return 1
+    else:
+        return 0
+
+    
