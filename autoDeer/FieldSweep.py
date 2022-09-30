df28b0ac6d14714995c6bf551c5246ff9a7786a9
Hugo Karas <hkaras@ethz.ch>
2022-09-30 16:33:05 +0200

    More file with flake8 support + Bug Fixes -Flake8 support   - Field Sweep   - Relaxation   - Resonator Profile - Bug Fixes:  - Fix autofit limits

--- autoDeer/FieldSweep.py
+++ autoDeer/FieldSweep.py
@@ -9,19 +9,20 @@ class FieldSweep():
     def __init__(self) -> None:
         pass
 
-    def import_from_bruker(self,file_path)->None:
+    def import_from_bruker(self, file_path) -> None:
 
-        t,V = deerload(file_path, False, True)
+        t, V = deerload(file_path, False, True)
         self.axis = t
         self.data = V
         pass 
 
-    def import_from_numpy(self,axis:np.ndarray,spectrum:np.ndarray) -> None:
+    def import_from_numpy(self, axis: np.ndarray, spectrum: np.ndarray)\
+            -> None:
         self.axis = axis
         self.data = spectrum
         pass
 
-    def import_from_dataclass(self,dataclass) -> None:
+    def import_from_dataclass(self, dataclass) -> None:
         self.axis = dataclass.time
         self.data = dataclass.data
         pass
@@ -32,9 +33,9 @@ class FieldSweep():
 
         return self.max_field
 
-    def calc_gyro(self,det_frq):
+    def calc_gyro(self, det_frq):
 
-        if not hasattr(self,"max_field"):
+        if not hasattr(self, "max_field"):
             self.find_max()
         
         self.gyro = det_frq/self.max_field
@@ -43,23 +44,22 @@ class FieldSweep():
         self.fs_x = det_frq - self.gyro*self.axis
         return self.gyro
 
-    def plot(self,norm=True)->Figure:
+    def plot(self, norm=True) -> Figure:
 
-        if norm == True:
+        if norm is True:
             data = self.data
             data /= np.max(np.abs(data))
 
-
         fig, ax = plt.subplots()
-        ax.plot(self.axis,np.abs(data),label='real')
-        ax.plot(self.axis,np.real(data),label='real')
-        ax.plot(self.axis,np.imag(data),label='imag')
+        ax.plot(self.axis, np.abs(data), label='real')
+        ax.plot(self.axis, np.real(data), label='real')
+        ax.plot(self.axis, np.imag(data), label='imag')
         ax.legend()
         ax.set_xlabel('Field G')
         ax.set_ylabel('Normalised Amplitude')
-        if hasattr(self,"max_field"):
-            min_value = np.min(np.hstack([np.real(data),np.imag(data)]))
-            max_value = np.min(np.hstack([np.real(data),np.imag(data)]))
-            ax.vlines(self.max_field,min_value,max_value,label="Maximum")
+        if hasattr(self, "max_field"):
+            min_value = np.min(np.hstack([np.real(data), np.imag(data)]))
+            max_value = np.min(np.hstack([np.real(data), np.imag(data)]))
+            ax.vlines(self.max_field, min_value, max_value, label="Maximum")
 
         return fig
--- autoDeer/Relaxation.py
+++ autoDeer/Relaxation.py
@@ -1,79 +1,82 @@
-from attr import has
 from matplotlib.figure import Figure
 import numpy as np
-from deerlab import deerload,noiselevel
+from deerlab import deerload, noiselevel
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+
+# ==
+# ===========================================================================
+
 
 class Carr_Purcell:
 
     def __init__(self) -> None:
         pass
 
-    def import_from_bruker(self,file_path)->None:
+    def import_from_bruker(self, file_path) -> None:
 
-        t,V = deerload(file_path, False, False)
+        t, V = deerload(file_path, False, False)
         self.axis = t
         self.data = V
         pass 
 
-    def import_from_numpy(self,axis:np.ndarray,spectrum:np.ndarray) -> None:
+    def import_from_numpy(
+            self, axis: np.ndarray, spectrum: np.ndarray) -> None:
         self.axis = axis
         self.data = spectrum
         pass
 
-    def import_from_dataclass(self,dataclass) -> None:
+    def import_from_dataclass(self, dataclass) -> None:
         self.axis = dataclass.time
         self.data = dataclass.data
         pass
     
-    def fit(self,type = "mono"):
+    def fit(self, type="mono"):
 
         data = np.abs(self.data)
         data /= np.max(data)
 
         if type == "mono":
-            self.func = lambda x,a,b,e: a*np.exp(-b*x**e)
-            p0 = [1,1,2]
-            bounds = ([0,0,-10,0],[2,1000,10,1000])
+            self.func = lambda x, a, b, e: a*np.exp(-b*x**e)
+            p0 = [1, 1, 2]
+            # bounds = ([0, 0, -10, 0],[2, 1000, 10, 1000])
         else:
             raise ValueError("Type must be one of: mono")
         
-        self.fit_result = curve_fit(self.func,self.axis,data,p0=p0)
+        self.fit_result = curve_fit(self.func, self.axis, data, p0=p0)
         pass
 
+    def plot(self, norm=True) -> Figure:
 
-    def plot(self,norm=True) -> Figure:
-
-        if norm == True:
+        if norm is True:
             data = np.abs(self.data)
             data /= np.max(data)
 
         fig, ax = plt.subplots()
-        if hasattr(self,"fit_result"):
-            ax.plot(self.axis,self.func(self.axis,*self.fit_result[0]),label='fit')
-            ax.plot(self.axis,data,label='data')
+        if hasattr(self, "fit_result"):
+            ax.plot(self.axis, self.func(
+                self.axis, *self.fit_result[0]), label='fit')
+            ax.plot(self.axis, data, label='data')
             ax.legend()
         else:
-            ax.plot(self.axis,data,label='data')
-
+            ax.plot(self.axis, data, label='data')
 
         ax.set_xlabel('Time / us')
         ax.set_ylabel('Normalised Amplitude')
         return fig
 
-    def find_optimal(self,shrt,averages):
-        time_per_point = shrt * averages
+    def find_optimal(self, shrt, averages):
+        # time_per_point = shrt * averages
 
         data = np.abs(self.data)
         data /= np.max(data)
 
         self.noise = noiselevel(data)
-        data_snr = data/self.noise
-        data_snr_avgs = data_snr/np.sqrt(averages)
+        data_snr = data / self.noise
+        data_snr_avgs = data_snr / np.sqrt(averages)
 
         # Assume 16ns time step
-        dt = 16
+        # dt = 16
         # Target time
         target_time = 2 * 3600
 
