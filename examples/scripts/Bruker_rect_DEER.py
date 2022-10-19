2d85b5433cdd80e4ee999c376b62da014f3bb857
Hugo Karas <hkaras@ethz.ch>
2022-10-19 16:06:57 +0200

    Improved example

--- examples/scripts/Bruker_rect_DEER.py
+++ examples/scripts/Bruker_rect_DEER.py
@@ -2,9 +2,10 @@
 autoDEER MPFU
 -------------
 
-Running an automated rectangular DEER experiment on a BRUKER spectrometer using MPFU channels
----------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------
+Running an automated rectangular DEER experiment on a BRUKER spectrometer using
+ MPFU channels
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 """
 import numpy as np
@@ -25,19 +26,10 @@ folder = "2022_10_03_autoDEER_rect_FUS_attempt2"
 # %%
 
 xepr = api()  # This can only be run once per kernel
-xepr.find_Xepr()
-xepr.find_cur_exp()
-xepr.find_hidden()
+xepr.connect()
 
 # This sets up the configuration file of spectrometer
-if xepr.hidden['PlsPrgCalDbName'].value != 'Q_1101004':
-    xepr.hidden['PlsPrgCalDbName'].value = 'Q_1101004'
-    xepr.hidden['PlsPrgCalDbLoad']
-    xepr.hidden['ApplyCfg']
-    print("Changed config file")
-else:
-    print("Not changed config file")
-
+xepr.set_config_file('Q_1101004')
 # %%
 # Measuring a first EDFS
 
@@ -63,49 +55,74 @@ exp.run_general(
 
 # Analyse EDFS
 
-fs_data = xepr.acquire_scan()
-fs = FieldSweep()
-fs.import_from_dataclass(fs_data)
+fs_dataset = xepr.acquire_scan()
+fs = FieldSweep(fs_dataset)
 fs.find_max()
+gyro_exp = fs.calc_gyro(xepr.get_counterfreq())
+print(fs.gyro)
 
 xepr.set_field(fs.max_field)
 Bc = xepr.get_field()
 fc = xepr.get_counterfreq()
 
 fs.plot()
-gyro_exp = fs.calc_gyro(xepr.get_counterfreq())
-print(fs.gyro)
-xepr.xepr_save(f"/home/xuser/xeprFiles/Data/HUKA/2022/{folder}/({project})_({sample})_(EDFS_Q)_init")
+xepr.xepr_save(
+    f"/home/xuser/xeprFiles/Data/HUKA/2022/{folder}/({project})_({sample})_"
+    "(EDFS_Q)_init")
 """0.002816783144203381 """
 
 # %%
 # Perform resonator profile
+
+exp.run_general(
+    xepr,
+    ["/PulseSpel/param_opt"],
+    ["nutation", "ELDOR nut"],
+    {"PhaseCycle": True, "ReplaceMode": False},
+    {"p0": 16, "p1": 16, "h": 20, "n": 1, "d0": d0-32, "dim7": 128}, False)
+xepr.set_attenuator('ELDOR', 0)
+xepr.set_ELDOR_freq(fc)
+
+
 t, nutation_data = exp.get_nutations(
     xepr,
     nu=[33.8, 34.4],
     field=[fc, Bc],
     step=20e-3)
 
-rp = resonatorProfile()
-res_prof = rp.calc_res_prof(nutation_data, dt=t[1]-t[0], freq_lim=[33.8, 34.4])
+rp = resonatorProfile(nutation_data[:, 1:], nutation_data[:, 0], t[1]-t[0])
+res_prof = rp.calc_res_prof(freq_lim=[33.8, 34.4])
 rp.autofit([33.8, 34.4])
 rp.calculate_shape(res_prof, None)
-rp.calc_IF_prof(2, rp.fc, type='fit')
+# rp.calc_IF_prof(2, rp.fc, type='fit')
 rp.res_prof_plot([33.8, 34.4])
 
 savedata = {"time": t, "Nutation Data": nutation_data}
-savemat("/home/xuser/xeprFiles/Data/HUKA/2022/{folder}/({project})_({sample})_(ResPro)_init.mat",savedata)
+savemat(
+    "/home/xuser/xeprFiles/Data/HUKA/2022/{folder}/({project})_({sample})_"
+    "(ResPro)_init.mat", savedata)
 
 # %%
 # Tune ELDOR Channel
 # DEER frequencies
 fpump = fc + 0.01
 fobs = fc - 0.04
-xepr.set_ELDOR_freq(fpump)
 
 # %%
 # DEER time
-# Setup DEER channels
+
+# Setup DEER pump channels
+xepr.set_freq(fpump)
+xepr.set_field(fpump/gyro_exp)
+xepr.set_ELDOR_freq(fpump)
+tune = exp.MPFUtune(xepr, "Hahn", 32, 660)
+tune.tune({'+<x>': "R+", '-<x>': "R-"}, tol=1, bounds=[20, 55])
+eltune = exp.ELDORtune(xepr, 650, 32)
+eltune._setup_exp()
+eldor_att = eltune.tune(32)
+xepr.set_attenuator(eldor_att)
+
+# Setup DEER obs channels
 xepr.set_freq(fobs)
 xepr.set_field(fobs/gyro_exp)
 tune = exp.MPFUtune(xepr, "Hahn", 32, 660)
@@ -113,8 +130,7 @@ tune.tune({'+<x>': "R+", '-<x>': "R-"}, tol=1, bounds=[20, 55])
 tune = exp.MPFUtune(xepr, "Hahn", 16, 660)
 tune.tune({'-<y>': "R+"}, tol=1, bounds=[30, 65])
 
-xepr.set_ELDOR_freq(fpump)
-xepr.set_field((fpump)/gyro_exp)
+xepr.set_field((fc)/gyro_exp)
 
 # %% 
 # 4 pulse DEER
