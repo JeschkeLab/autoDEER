f03503d319cee271712131215f3ba4730cabf6c7
Hugo Karas <hkaras@ethz.ch>
2022-11-10 11:32:57 +0100

    Fixed test

--- test/test_xepr.py
+++ test/test_xepr.py
@@ -1,14 +1,11 @@
 # Here is a collection of pytests for the Xepr module. This is very limited due to the obvious lack of a spectrometer.
 
 import autoDeer.hardware.xepr_experiments as x_exp
-import importlib
 import re
-
-MODULE_DIR = importlib.util.find_spec('autoDeer').submodule_search_locations[0]
 
 
 def test_change_dimensions():
-    path = MODULE_DIR+"/PulseSpel/HUKA_DEER_AWG.exp"
+    path = "autoDeer/PulseSpel/HUKA_DEER_AWG.exp"
     dim = 8
     new_dim = 256
     x_exp.change_dimensions(path, dim, new_dim)
@@ -24,6 +21,4 @@ def test_change_dimensions():
             match = int(output[0])
 
     assert match == new_dim
-
-    
 
