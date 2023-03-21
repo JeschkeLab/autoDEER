d89c119c75ce22169ba0ec2a38f61cd17e4ab41e
Hugo Karas <hkaras@ethz.ch>
2023-03-21 22:41:38 +0100

    Init fix

--- autodeer/__init__.py
+++ autodeer/__init__.py
@@ -1,11 +1,8 @@
 from ._version import __version__
-from .Logging import *
-from .TwoD_Experiment import *
 from .tools import eprload
-from .FieldSweep import FieldSweep
-from .ResPro import ResonatorProfile
-from .home_built_func import get_deer, find_AWG_deer_files, uwb_load,\
-    calc_perceived_freq
-from .DEER_analysis import std_deer_analysis, IdentifyROI, remove_echo,\
-    calc_optimal_deer_frqs, plot_optimal_deer_frqs
-from .Relaxation import Carr_Purcell
+from .FieldSweep import FieldSweepAnalysis
+from .ResPro import ResonatorProfileAnalysis
+from .DEER_analysis import DEERanalysis, DEERanalysis_plot, calc_optimal_deer_frqs, plot_optimal_deer_frqs, IdentifyROI
+from .Relaxation import CarrPurcellAnalysis
+from .classes import *
+from .sequences import *
