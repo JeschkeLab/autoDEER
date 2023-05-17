250d6f63995656b41eb3a53b3a390788b52e0700
Hugo Karas <hkaras@ethz.ch>
2023-05-17 14:25:19 +0200

    Added sequence to be saved in dataset

--- autodeer/hardware/ETH_awg.py
+++ autodeer/hardware/ETH_awg.py
@@ -42,6 +42,7 @@ class ETH_awg_interface(Interface):
         self.awg_freq = awg_freq
         self.dig_rate = dig_rate
         self.pulses = {}
+        self.cur_exp = None
         pass
 
     def connect(self, session=None):
@@ -108,6 +109,7 @@ class ETH_awg_interface(Interface):
                 Matfile = loadmat(path, simplify_cells=True, squeeze_me=True)
                 data = uwb_load(Matfile, options=options, verbosity=verbosity)
                 data.LO = Parameter("LO", data.params['LO']+self.awg_freq, unit="GHz", description="Total local oscilator frequency")
+                data.sequence = self.cur_exp
             except OSError:
                 time.sleep(10)
             else:
