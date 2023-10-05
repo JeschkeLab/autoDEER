034717e45dae705b5c0c91fece36e9a045297093
Hugo Karas <hkaras@ethz.ch>
2023-10-05 16:45:19 +0200

    Add reptime experiment and analysis

--- autodeer/__init__.py
+++ autodeer/__init__.py
@@ -3,10 +3,11 @@ from .tools import eprload
 from .FieldSweep import FieldSweepAnalysis
 from .ResPro import ResonatorProfileAnalysis
 from .DEER_analysis import *
-from .Relaxation import CarrPurcellAnalysis
+from .Relaxation import *
 from .classes import *
 from .sequences import *
 from .pulses import *
 from .criteria import *
 from .dataset import Dataset
 from .reporter import Reporter
+from .gui.main import autoDEERUI
