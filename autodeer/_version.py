716f188f23bc3ddbcdba7834df2ef34211774417
Hugo Karas <hkaras@ethz.ch>
2024-10-01 17:00:53 +0200

    Better versioning

--- autodeer/_version.py
+++ autodeer/_version.py
@@ -1,1 +1,16 @@
-__version__ = "0.8"
+__version__ = "0.9"
+
+try: 
+    import git
+    import os
+
+    branch = git.Repo(os.path.dirname(__file__),search_parent_directories=True).active_branch.name
+    commit = git.Repo(os.path.dirname(__file__),search_parent_directories=True).head.commit.hexsha
+
+    if branch == 'main':
+        __version__ = __version__
+    else:
+        __version__ = f"{__version__}-{branch}-{commit[:7]}"
+
+except:
+    __version__ = __version__
